6439e77e97481959d611cc637fd7b59290461d63
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-07-01 00:39:48 -0500

    Actualizar ambiente de producción

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGF5onEmdMwAa-ASgLK7By1cNViCNoMdg",
+    authDomain: "prueba-ebb92.firebaseapp.com",
+    databaseURL: "https://prueba-ebb92.firebaseio.com",
+    projectId: "prueba-ebb92",
+    storageBucket: "",
+    messagingSenderId: "968181811941",
+    appId: "1:968181811941:web:3f57add6e897842c"
+  }
 };
