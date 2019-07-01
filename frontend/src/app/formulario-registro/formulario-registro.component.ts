91242cff98b43714caf9f5c654558348a33d108f
Camilo Restrepo Ramírez <camilo17746@gmail.com>
2019-07-01 00:22:00 -0500

    Escoger color entre una lista de colores para cambiar el tema de la aplicación

--- frontend/src/app/formulario-registro/formulario-registro.component.ts
+++ frontend/src/app/formulario-registro/formulario-registro.component.ts
@@ -13,41 +13,41 @@ export class FormularioRegistroComponent implements OnInit {
   newPerson: Persona;
   clientes: any;
   resultado: Persona[];
-  constructor( private service: ObtenerClientesService, private db :AngularFireDatabase ) { }
+  constructor(private service: ObtenerClientesService, private db: AngularFireDatabase) { }
 
   ngOnInit() {
     this.newPerson = new Persona();
   }
 
-  validarDatos(){
-    if(this.newPerson.identification===undefined){
-      this.mensaje("Error!","Identification not entered",2);
-    }else if(this.newPerson.birthdate===undefined){
-      this.mensaje("Error!","Birthdate not entered",2);
-    }else if(this.newPerson.firstname===undefined){
-      this.mensaje("Error!","Firstname not entered",2);
-    }else if(this.newPerson.lastname===undefined){
-      this.mensaje("Error!","Lastname not entered",2);
-    }else{
+  validarDatos() {
+    if (this.newPerson.identification === undefined) {
+      this.mensaje("Error!", "Identification not entered", 2);
+    } else if (this.newPerson.birthdate === undefined) {
+      this.mensaje("Error!", "Birthdate not entered", 2);
+    } else if (this.newPerson.firstname === undefined) {
+      this.mensaje("Error!", "Firstname not entered", 2);
+    } else if (this.newPerson.lastname === undefined) {
+      this.mensaje("Error!", "Lastname not entered", 2);
+    } else {
       this.validaciones();
     }
   }
 
   ingresarUsuario() {
     this.db.list('/clients').push(this.newPerson);
-   }
+  }
 
-  validaciones(){
+  validaciones() {
     this.service.obtenerClientes().subscribe(res => {
       this.clientes = res;
       this.resultado = Object.keys(this.clientes).map(e => this.clientes[e]);
-      if(this.resultado.find(cliente => cliente.identification==this.newPerson.identification)){
-        this.mensaje("Error!","Client already registered in the database",2);
-      }else if(!this.esMayorDeEdad()){
-        this.mensaje("Error!","You are not of legal age",2);
-      }else{
+      if (this.resultado.find(cliente => cliente.identification == this.newPerson.identification)) {
+        this.mensaje("Error!", "Client already registered in the database", 2);
+      } else if (!this.esMayorDeEdad()) {
+        this.mensaje("Error!", "You are not of legal age", 2);
+      } else {
         this.ingresarUsuario();
-        this.mensaje("Excellent!","You are registered",1);
+        this.mensaje("Excellent!", "You are registered", 1);
       }
     })
 
@@ -57,9 +57,9 @@ export class FormularioRegistroComponent implements OnInit {
     Swal.fire(titulo, mensaje, (tipoMensaje == 1) ? 'success' : 'error')
   }
 
-  esMayorDeEdad(): boolean{
-    let age = (new Date().getTime() - new Date(<string>this.newPerson.birthdate).getTime())/(1000 * 3600 * 24 * 365);
-    if(age < 18){
+  esMayorDeEdad(): boolean {
+    let age = (new Date().getTime() - new Date(<string>this.newPerson.birthdate).getTime()) / (1000 * 3600 * 24 * 365);
+    if (age < 18) {
       return false;
     }
     return true
